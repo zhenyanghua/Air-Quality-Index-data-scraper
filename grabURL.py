d54eba49e7787f0f29eeae219890388e700b44a0
ichelite <edison.hua@gmail.com>
2014-07-15 09:54:29 -0400

    Source change
    Code is revised to adapt newest source format.

--- grabURL.py
+++ grabURL.py
@@ -19,33 +19,32 @@ class GrabURL:
         
         soup=BeautifulSoup(html)
         
-        body=soup.find("h1",text="China").findNext("div","whitebody")
-        city=body.find_all('div',style="width:80%")
+        body=soup.find("div",text="CHINA").findNext('div')
+        station=body.find_all('a')
         
         i=0  
-        for station in city:
-            for each in station.find_all('a'):
-                s=each.text.split(' (')[0].encode('utf8')
-                u=s.decode('utf8')
-                try:
-                    cStationName=repr(u).split("'")[1]
-                    cCNName=each.text.split('(')[1].encode('utf8').split(')')[0]
-                    cURL=each.get('href')
-                    self.cur.execute("INSERT INTO url VALUES (%s,%s,%s);",(cStationName,cCNName,cURL,))
-                    i+=1
-                except:
-                    pass
-                #try:
-                #    self.cur.execute("INSERT INTO url VALUES (%s,%s,%s)",(repr(u).split("'")[1],each.text.split('(')[1].encode('utf8').split(')')[0],each.get('href')))
-                #except:
-                #    pass
+        for each in station:
+            print each
+            s=each.text.split(' (')[0].encode('utf8')
+            u=s.decode('utf8')
+            
+            cStationName=repr(u).split("'")[1]
+            cCNName=None
+            try:  
+                cCNName=each.text.split('(')[1].encode('utf8').split(')')[0]
+            except:
+                pass
+            cURL=each.get('href')
+            self.cur.execute("INSERT INTO url VALUES (%s,%s,%s);",(cStationName,cCNName,cURL,))
+            i+=1
+
         self.rowcount=i
         self.conn.commit()
         
         self.cur.close()
         self.conn.close()
         
-        print 'URLs have been grabbed into url table\n'
+        print str(i)+' URLs have been grabbed into url table\n'
 
 
 
